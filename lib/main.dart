06f3ec578919d87b2d3dc9ce50f0a7a8e173659e
Jonathan Romero <n.bjhon@gmail.com>
2020-03-30 18:27:28 -0500

    add routes

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:componentes/src/routes/routes.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      //home: HomePage());
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        print('ruta llamada: ${settings.name}');
+
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
+      },
+    );
+  }
+}
