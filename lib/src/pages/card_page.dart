f1fa6a0c236cbe185086ec7970737f7412e7ec7e
Jonathan Romero <n.bjhon@gmail.com>
2020-04-05 13:42:28 -0500

    card list view

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Cards'),
+        ),
+        body: ListView(
+          padding: EdgeInsets.all(10.0),
+          children: <Widget>[
+            _cardTipo1(),
+            SizedBox(height: 30.0),
+            _cardTipo2(),
+            SizedBox(height: 30.0),
+            _cardTipo1(),
+            SizedBox(height: 30.0),
+            _cardTipo2(),
+            SizedBox(height: 30.0),
+            _cardTipo1(),
+            SizedBox(height: 30.0),
+            _cardTipo2(),
+            SizedBox(height: 30.0),
+            _cardTipo1(),
+            SizedBox(height: 30.0),
+            _cardTipo2(),
+          ],
+        ));
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el titulo de esta tarjeta'),
+            subtitle: Text(
+                'Aqui estamos con la descripcion del titulo de esta tarjeta'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('ok'),
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    final card = Container(
+      // clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+
+          // Image(
+
+          // ),
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('Pragser Wildsee, Italy'))
+        ],
+      ),
+    );
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                spreadRadius: 2.0,
+                offset: Offset(2.0, 10.0))
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
+      ),
+    );
+  }
+}
